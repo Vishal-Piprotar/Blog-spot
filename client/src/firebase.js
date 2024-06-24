3f5ebbe2c0b43dbaa605aefe123370d422d67fa6
Vishal Piprotar <150599739+Vishal-Piprotar@users.noreply.github.com>
2024-06-25 01:17:45 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,13 @@
-
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAa8IPMyuj5O8sIbUlmsFy0YgM2PH7z1SY",
-  authDomain: "blog-application-mern.firebaseapp.com",
-  projectId: "blog-application-mern",
-  storageBucket: "blog-application-mern.appspot.com",
-  messagingSenderId: "587991180539",
-  appId: "1:587991180539:web:0c892f81209b29e2dcbc5e",
-  measurementId: "G-37FJLDB27S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
