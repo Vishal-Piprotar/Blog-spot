b0ab8272609dfe37fc0b309175ca11c024a14f81
Vishal Piprotar <150599739+Vishal-Piprotar@users.noreply.github.com>
2024-06-25 01:35:39 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,15 @@
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envDir: '.',
-  envPrefix: 'VITE_',
   server: {
     proxy: {
       '/api': {
-        target: 'https://firebasestorage.googleapis.com',
-        changeOrigin: true,
+        target: 'http://localhost:3000',
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-        },
       },
-      // Add your additional proxy configurations here if needed
     },
   },
+  plugins: [react()],
 });
