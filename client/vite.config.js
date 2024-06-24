5d6c017321015497ff84a89588d119012b2b9f7a
Vishal-Piprotar <piprotarvishal20@gmail.com>
2024-06-25 01:43:47 +0530

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,10 @@
+
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import dotenv from 'dotenv';
+import react from '@vitejs/plugin-react';
 
-dotenv.config();
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
-  define: {
-    'process.env': process.env,
-  },
+  envDir: '.',
+  envPrefix: 'VITE_',
+  
 });
